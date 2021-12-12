e1705bdd97f0fb817514dbc2ddbc77a9c21dfa63
Adam Pridmore <adam_pridomre@hotmmail.com>
2021-12-12 22:48:50 +0000

    Refactorings

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -2,57 +2,55 @@ package com.gildedrose
 
 class GildedRose(val items: Array[Item]) {
 
+  def increaseQualityRules(item: Item){
+    if (item.quality < 50) {
+      item.quality = item.quality + 1
 
-  def updateQuality() {
-    for (i <- 0 until items.length) {
-      if (!items(i).name.equals("Aged Brie")
-        && !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (items(i).quality > 0) {
-          if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-            items(i).quality = items(i).quality - 1
-          }
+      if (item.isBackstagePass) {
+        if (item.sellIn < 11 && item.quality < 50) {
+          item.quality = item.quality + 1
         }
-      } else {
-        if (items(i).quality < 50) {
-          items(i).quality = items(i).quality + 1
 
-          if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).sellIn < 11) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
-
-            if (items(i).sellIn < 6) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
-          }
-        }
-      }
-
-      if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-        items(i).sellIn = items(i).sellIn - 1
-      }
-
-      if (items(i).sellIn < 0) {
-        if (!items(i).name.equals("Aged Brie")) {
-          if (!items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).quality > 0) {
-              if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-                items(i).quality = items(i).quality - 1
-              }
-            }
-          } else {
-            items(i).quality = items(i).quality - items(i).quality
-          }
-        } else {
-          if (items(i).quality < 50) {
-            items(i).quality = items(i).quality + 1
-          }
+        if (item.sellIn < 6 && item.quality < 50) {
+          item.quality = item.quality + 1
         }
       }
     }
+  }
+
+  def reduceQualityRules(item: Item){ 
+    if (item.quality > 0) {
+      if (!item.isLegendary) {
+        item.quality = item.quality - 1
+      }
+    }
+  }
+
+  def processItem(item: Item) {
+   if (item.isAgedBrie || item.isBackstagePass) {
+      increaseQualityRules(item)
+    } else {
+      reduceQualityRules(item)
+    }
+
+    if (!item.isLegendary) {
+      item.sellIn = item.sellIn - 1
+    }
+
+    if (item.outOfDate) {
+      if (item.isAgedBrie) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1
+        }
+      } else if (item.isBackstagePass) {
+        item.quality = 0
+      } else if (item.quality > 0 && !item.isLegendary) {
+        item.quality = item.quality - 1
+      }
+    }
+  }
+ 
+  def updateQuality() {
+    items.foreach(processItem)
   }
 }
--- scala/src/main/scala/com/gildedrose/Item.scala
+++ scala/src/main/scala/com/gildedrose/Item.scala
@@ -1,5 +1,12 @@
 package com.gildedrose
 
-class Item(val name: String, var sellIn: Int, var quality: Int) {
+// TODO: Make imutable
+class Item(val name: String, var sellIn: Int, var quality: Int){
+  val isLegendary = name.equals("Sulfuras, Hand of Ragnaros")
 
+  def isBackstagePass = name.equals("Backstage passes to a TAFKAL80ETC concert")
+  
+  def isAgedBrie = name.equals("Aged Brie")
+
+  def outOfDate = sellIn < 0
 }
