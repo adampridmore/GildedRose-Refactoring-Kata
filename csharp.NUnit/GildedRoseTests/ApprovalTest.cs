6198593a1a1b470e01ea25d1fa64ff477aed3295
adampridmore <320922+adampridmore@users.noreply.github.com>
2025-03-16 20:59:12 +0000

    Add more approvals tests

--- csharp.NUnit/GildedRoseTests/ApprovalTest.cs
+++ csharp.NUnit/GildedRoseTests/ApprovalTest.cs
@@ -43,4 +43,73 @@ public class ApprovalTest
 
         return Verifier.Verify(output);
     }
+    
+    [Test]
+    public Task AgesBrieItemThirtyDays()
+    {
+        var fakeOutput = new StringBuilder();
+        Console.SetOut(new StringWriter(fakeOutput));
+        Console.SetIn(new StringReader($"a{Environment.NewLine}"));
+
+        var items = new List<Item>
+        {
+            new Item { Name = "Aged Brie", SellIn = 10, Quality = 20 }
+        };
+        Program.ProcessItems(items, 30);
+        var output = fakeOutput.ToString();
+
+        return Verifier.Verify(output);
+    }
+    
+    [Test]
+    public Task LegendaryItemThirtyDays()
+    {
+        var fakeOutput = new StringBuilder();
+        Console.SetOut(new StringWriter(fakeOutput));
+        Console.SetIn(new StringReader($"a{Environment.NewLine}"));
+
+        var items = new List<Item>
+        {
+            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+        };
+        Program.ProcessItems(items, 30);
+        var output = fakeOutput.ToString();
+
+        return Verifier.Verify(output);
+    }
+    
+    [Test]
+    public Task BackstagePassThirtyDays()
+    {
+        var fakeOutput = new StringBuilder();
+        Console.SetOut(new StringWriter(fakeOutput));
+        Console.SetIn(new StringReader($"a{Environment.NewLine}"));
+
+        var items = new List<Item>
+        {
+            new Item {  Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 },
+        };
+        Program.ProcessItems(items, 30);
+        var output = fakeOutput.ToString();
+
+        return Verifier.Verify(output);
+    }
+    
+    [Test]
+    [Ignore("WIP")]
+    public Task ConjuredItemThirtyDays()
+    {
+        var fakeOutput = new StringBuilder();
+        Console.SetOut(new StringWriter(fakeOutput));
+        Console.SetIn(new StringReader($"a{Environment.NewLine}"));
+
+        var items = new List<Item>
+        {
+            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+        };
+        Program.ProcessItems(items, 30);
+        var output = fakeOutput.ToString();
+
+        return Verifier.Verify(output);
+    }
 }
