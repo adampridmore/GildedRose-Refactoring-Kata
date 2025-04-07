aa86de80069f6720d0415cb851935db57e7cf3b0
adampridmore <320922+adampridmore@users.noreply.github.com>
2025-04-07 09:36:54 +0100

    Small refactorings

--- csharp.NUnit/GildedRose/Program.cs
+++ csharp.NUnit/GildedRose/Program.cs
@@ -3,45 +3,41 @@ using System.Collections.Generic;
 
 namespace GildedRoseKata;
 
-public class Program
+public static class Program
 {
     public static void Main(string[] args)
     {
-        
-        
-        
         Console.WriteLine("OMGHAI!");
 
         IList<Item> items = new List<Item>
         {
-            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-            new Item
+            new() {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+            new() {Name = "Aged Brie", SellIn = 2, Quality = 0},
+            new() {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+            new() {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+            new() {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+            new()
             {
                 Name = "Backstage passes to a TAFKAL80ETC concert",
                 SellIn = 15,
                 Quality = 20
             },
-            new Item
+            new()
             {
                 Name = "Backstage passes to a TAFKAL80ETC concert",
                 SellIn = 10,
                 Quality = 49
             },
-            new Item
+            new()
             {
                 Name = "Backstage passes to a TAFKAL80ETC concert",
                 SellIn = 5,
                 Quality = 49
             },
-            // this conjured item does not work properly yet
-            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            new() {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
         };
 
-        int days = 2;
+        var days = 2;
         if (args.Length > 0)
         {
             days = int.Parse(args[0]) + 1;
@@ -58,9 +54,9 @@ public class Program
         {
             Console.WriteLine("-------- day " + i + " --------");
             Console.WriteLine("name, sellIn, quality");
-            for (var j = 0; j < items.Count; j++)
+            foreach (var item in items)
             {
-                Console.WriteLine(items[j].Name + ", " + items[j].SellIn + ", " + items[j].Quality);
+                Console.WriteLine(item.Name + ", " + item.SellIn + ", " + item.Quality);
             }
             Console.WriteLine("");
             app.UpdateQuality();
